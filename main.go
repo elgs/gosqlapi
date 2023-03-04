4dd84d339db54bb6bd1b7f6a68a994d005256768
Qian Chen <elgs1980@hotmail.com>
2023-03-03 16:55:59 -0800

    Update.

--- main.go
+++ main.go
@@ -8,12 +8,7 @@ import (
 	"os"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	_ "github.com/mattn/go-sqlite3"
-	_ "github.com/microsoft/go-mssqldb"
-	_ "github.com/sijms/go-ora/v2"
-	_ "modernc.org/sqlite"
 )
 
 func init() {
