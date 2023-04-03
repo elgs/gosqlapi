3d2bf760c9508865748561cc81b27119f60564c2
Qian Chen <elgs1980@hotmail.com>
2023-04-02 20:08:47 -0700

    Merge branch 'master' into pq,

--- release.sh
+++ release.sh
@@ -21,14 +21,14 @@ for branch in "${branches[@]}"
 do
     git checkout "$branch"
     git pull origin "$branch"
-    git merge master --no-ff --no-commit
-    for file in "${do_not_merge[@]}"
-    do
-        git reset HEAD -- "$file"
-        git checkout -- "$file"
-    done
-    GOPROXY=direct go get -u
-    go mod tidy
+    # git merge master --no-ff --no-commit
+    # for file in "${do_not_merge[@]}"
+    # do
+    #     git reset HEAD -- "$file"
+    #     git checkout -- "$file"
+    # done
+    # GOPROXY=direct go get -u
+    # go mod tidy
     git commit -am "Merge branch 'master' into $branch"
     git push origin "$branch"
 done
