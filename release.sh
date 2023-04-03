296a57f067bb1fa273a6fbc3e2fa77843d313901
Qian Chen <elgs1980@hotmail.com>
2023-04-02 20:09:22 -0700

    Update.

--- release.sh
+++ release.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 declare -a branches=(
-    # "all"
-    # "mysql" 
+    "all"
+    "mysql" 
     "pq",
-    # "pgx"
-    # "sqlite"
-    # "sqlite3"
-    # "sqlserver"
-    # "oracle"
+    "pgx"
+    "sqlite"
+    "sqlite3"
+    "sqlserver"
+    "oracle"
 )
 
 declare -a do_not_merge=(
@@ -21,14 +21,14 @@ for branch in "${branches[@]}"
 do
     git checkout "$branch"
     git pull origin "$branch"
-    # git merge master --no-ff --no-commit
-    # for file in "${do_not_merge[@]}"
-    # do
-    #     git reset HEAD -- "$file"
-    #     git checkout -- "$file"
-    # done
-    # GOPROXY=direct go get -u
-    # go mod tidy
+    git merge master --no-ff --no-commit
+    for file in "${do_not_merge[@]}"
+    do
+        git reset HEAD -- "$file"
+        git checkout -- "$file"
+    done
+    GOPROXY=direct go get -u
+    go mod tidy
     git commit -am "Merge branch 'master' into $branch"
     git push origin "$branch"
 done
